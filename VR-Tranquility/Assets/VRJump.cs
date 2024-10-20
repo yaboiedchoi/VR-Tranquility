c22f85a2bf132c6960400e13bffe610ac1f1e547
yaboiedchoi <ec2244@rit.edu>
2024-10-19 22:56:53 -0400

    added hands, item interaction (ray on left hand, direct on right), movement, rotation, jumping,

---
+++ VR-Tranquility/Assets/VRJump.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VRJump : MonoBehaviour
+{
+    [SerializeField] private InputActionReference jumpButton;
+    [SerializeField] private float jumpHeight = 2.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    private CharacterController _characterController;
+    private Vector3 _playerVelocity;
+
+    private void Awake() => _characterController = GetComponent<CharacterController>();
+
+    private void OnEnable() => jumpButton.action.performed += Jumping;
+    private void OnDisable() => jumpButton.action.performed -= Jumping;
+
+    private void Jumping(InputAction.CallbackContext obj)
+    {
+        if (!_characterController.isGrounded) return;
+        _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+    }
+
+    private void Update()
+    {
+        if (_characterController.isGrounded && _playerVelocity.y < 0)
+            _playerVelocity.y = 0f;
+
+
+        _playerVelocity.y += gravityValue * Time.deltaTime;
+        _characterController.Move(_playerVelocity * Time.deltaTime);
+    }
+}
